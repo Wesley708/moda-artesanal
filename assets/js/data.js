2a517b5b83434b41ec1e0c798a79749bb7385672
Wesley Alves do Nascimento <65147671+Wesley708@users.noreply.github.com>
2025-04-10 00:34:35 +0000

    Estrutura dos dados

---
+++ assets/js/data.js
@@ -0,0 +1,18 @@
+let vestidos = [
+    {
+        id : 1,
+        nome : 'Vestido Rosa',
+        descricao : "",
+        preco : 200,
+        image : ""    
+    },
+    {
+        id : 1,
+        nome : 'Vestido Rosa',
+        descricao : "",
+        preco : 200,
+        image : ""    
+    },
+
+]
+
